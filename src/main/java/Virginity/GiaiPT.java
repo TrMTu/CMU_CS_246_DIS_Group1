842eb99d47f0ade2d2f7c33b315b8fd49bf18737
TrMTu <minhtu.kun@gmail.com>
2022-04-27 07:35:24 +0100

    first commit

---
+++ src/main/java/Virginity/GiaiPT.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Virginity;
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+/**
+ *
+ * @author TrMTu
+ */
+public class GiaiPT {
+
+    public static String GiaiPTB2(double a, double b, double c) {
+        
+        return "";
+    }
+
+    public static String GiaiPTB3(double a, double b, double c, double d) {
+        
+        return "";
+    }
+    public static String GiaiHePT12(double a1,double a2,double b1, double b2, double c1, double c2){
+      
+        return "";
+    }
+    public static String GiaiHPT13(double a1, double a2, double a3,double b1, double b2, double b3, double c1, double c2, double c3, double d1, double d2, double d3){
+        
+        
+        return "";
+    }
+
+}
